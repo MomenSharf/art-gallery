e4ab4f3140f0ab1ee793c148f9cc61b570ec55bb
MomenSharf <mwmnshrfaldinps3@gmail.com>
2026-08-25 22:42:39 +0300

    update

--- lib/actions/logout.ts
+++ lib/actions/logout.ts
@@ -5,5 +5,10 @@ import { cookies } from "next/headers";
 export async function logoutManage() {
   const cookieStore = await cookies();
 
-  cookieStore.delete("manage_session");
+  cookieStore.delete({
+    name: "manage_session",
+    path: "/manage",
+  });
+
+  return { success: true };
 }
