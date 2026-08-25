29ae2a1b5f54d8f284fdccf3a6b50cb6d5930910
MomenSharf <mwmnshrfaldinps3@gmail.com>
2026-08-25 23:14:53 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,14 @@
 import ArtistIntroduction from "@/components/gallery/artist-introduction";
 import Gallery from "@/components/gallery/gallery";
 import GalleryHeader from "@/components/gallery/gallery-header";
-import prisma from "@/lib/prisma";
-
-const ARTIST_PROFILE_ID = "ArtistProfile";
+import { getArtworks } from "@/lib/actions/get-artworks";
+import { getProfile } from "@/lib/actions/get-profile";
 
 export default async function Home() {
-  const [artworks, profile] = await Promise.all([
-    prisma.artwork.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    }),
-
-    prisma.artistProfile.findUnique({
-      where: {
-        id: ARTIST_PROFILE_ID,
-      },
-    }),
-  ]);
-
+  const artworks = await getArtworks();
+  const profile = await getProfile();
   return (
-    <main
-      dir="rtl"
-      className="min-h-screen bg-[#f5f3ee] text-[#181816]"
-    >
+    <main dir="rtl" className="min-h-screen bg-[#f5f3ee] text-[#181816]">
       <GalleryHeader />
 
       <ArtistIntroduction profile={profile} />
